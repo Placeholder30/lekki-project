d68d4fe5333468797d94e0c06678e7d5b318be04
Malik <malikoabdulaziz@gmail.com>
2021-03-24 13:30:05 +0100

    add featured products section, markup and styles

--- client/src/components/home/FeaturedProducts.js
+++ client/src/components/home/FeaturedProducts.js
@@ -1,19 +1,89 @@
 import React from "react";
 import styled from "styled-components";
+import prod1 from "../assets/products/1.jpg";
+import prod2 from "../assets/products/2.jpg";
+import prod3 from "../assets/products/3.jpg";
 function FeaturedProducts() {
   return (
-    <Container>
+    <ProductContainer>
       <h2>
-        <span>Featured Products</span>
+        <span>Featured </span>
       </h2>
-    </Container>
+      <div className="cards">
+        <ProductCard id={1} />
+        <ProductCard id={2} />
+        <ProductCard id={3} />
+      </div>
+    </ProductContainer>
   );
 }
-
-const Container = styled.section`
+function ProductCard({ id }) {
+  return (
+    <CardContainer>
+      <div className="prod-image">
+        <img src={id === 1 ? prod1 : id === 2 ? prod2 : prod3} alt="watch" />
+      </div>
+      <p className="prod-name">Desi Avramovitz</p>
+      <p className="price">50.88</p>
+      <div className="view-prod">View Item {">"} </div>
+    </CardContainer>
+  );
+}
+const ProductContainer = styled.section`
   width: 100%;
+
   h2 {
-    text-align: center;
+    display: flex;
+    width: 100%;
+    text-transform: uppercase;
+    font-size: 2rem;
+    align-items: center;
+  }
+  h2::before,
+  h2::after {
+    content: " ";
+    width: 100%;
+    height: 1px;
+    background-color: #ea5f5f;
+  }
+  span {
+    font-size: 2rem;
+    margin: 0 3rem;
+    color: #ea5f5f;
+  }
+  .cards {
+    display: flex;
+    justify-content: space-around;
+  }
+`;
+
+const CardContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border: 1px solid #ccc;
+  border-radius: 2rem;
+  margin-top: 2rem;
+  font-size: 1.4rem;
+  padding: 2rem;
+  .price {
+    font-size: inherit;
+    color: red;
+    font-weight: 500;
+  }
+  .prod-name {
+    font-size: inherit;
+  }
+  .view-prod {
+    font-size: inherit;
+    border: 1px solid #ccc;
+    padding: 0.5rem;
+    &:hover {
+      cursor: pointer;
+    }
+  }
+  p {
+    margin: 0.5rem 0;
   }
 `;
 
